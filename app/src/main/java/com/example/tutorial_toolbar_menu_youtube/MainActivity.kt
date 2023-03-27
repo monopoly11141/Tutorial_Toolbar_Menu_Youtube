f62e586db206b57df244b0450dd23457468cc9d3
monopoly11141 <monopoly1114@vt.edu>
2023-03-27 13:18:13 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tutorial_toolbar_menu_youtube/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tutorial_toolbar_menu_youtube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
